a201ee61a5a5e546f1cfa88a3b692b224c27e859
astadipati <astadipati@otlook.com>
2021-07-12 06:22:58 +0700

    CRUD Operation

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -6,45 +6,98 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamp
 // @access  Public
 
-exports.getBootcamps = (req, res, next) =>{ //midleware function
-    res
-        .status(200)
-        .json({success: true, msg:'Show All Bootcamps', hello:req.hello}); //sekaligus akses middleware logger
-}  
+exports.getBootcamps = async (req, res, next) =>{ //midleware function
+    try {
+        const bootcamps = await Bootcamp.find();
+        res
+            .status(200)
+            .json({success: true, jml: bootcamps.length, data: bootcamps}); //sekaligus akses middleware logger
+    } catch (err) {
+        res
+            .status(400)
+            .json({success:false});
+    }
+}; 
 // @desc    Get single bootcamps
 // @route   GET /api/v1/bootcamp/:id
 // @access  Public
 
-exports.getBootcamp = (req, res, next) =>{ //midleware function
-    res.status(200).json({success: true, msg: `get single bootcamps ${req.params.id}`});
-}  
+exports.getBootcamp = async (req, res, next) =>{ //midleware function
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        // jika sesuai format tapi tidak ada data
+        if (!bootcamp) {
+            // kita kembalikan dengan return
+            return res.status(400).json({success: false, msg: `Tidak ada data`});
+        }
+        res
+            .status(200)
+            .json({success: true, data: bootcamp});
+    } catch (err) {
+        res
+            .status(400)
+            .json({success:false});
+    }
+    // res.status(200).json({success: true, msg: `get single bootcamps ${req.params.id}`});
+};  
 
 // @desc    Create a new bootcamp
 // @route   POST /api/v1/bootcamp
 // @access  Private after login
 
 exports.createBootcamp = async (req, res, next) =>{ //midleware function
-    const bootcamp = await Bootcamp.create(req.body);
-
-    res.status(201).json({
-        success: true,
-        data: bootcamp
-    });
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
+        
+    } catch (error) {
+        res
+        .status(400)
+        .json({success:false, msg:`Cek duplikat`});
+    }
     // console.log(req.body);
     // res.status(200).json({success: true, msg: 'Create new bootcamps'});
-}  
+};  
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamp:id
 // @access  Private
 
-exports.updateBootcamp = (req, res, next) =>{ //midleware function
-    res.status(200).json({success: true, msg: `Update bootcamps ${req.params.id}`});
-}  
+exports.updateBootcamp = async (req, res, next) =>{ //midleware function
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body,{
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({success: true, msg: `Update bootcamps ${req.params.id}`, data: bootcamp});
+        if (!bootcamp) {
+            return res.status(400).json({success: false, msg:`Gagal Update`});
+        }
+    } catch (error) {
+        res
+        .status(400)
+        .json({success:false, msg:`Gagal Melakukan Update ${req.params.id}`});
+    }
+};
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamp/:id
 // @access  private
 
-exports.deleteBootcamp = (req, res, next) =>{ //midleware function
-    res.status(200).json({success: true, msg: `Delete bootcamps ${req.params.id}`});
-}  
+exports.deleteBootcamp =async (req, res, next) =>{ //midleware function
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({success: true, msg: `Delete bootcamps ${req.params.id}`});
+        if (!bootcamp) {
+            return res.status(400).json({success: false, msg:`Gagal Hapus Data`});
+        }
+    } catch (error) {
+        res
+        .status(400)
+        .json({success:false, msg:`Gagal Menghapus Data ${req.params.id}`});
+    }
+};  
