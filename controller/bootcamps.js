b33677b59ff70a016e0e5ff55ce9fbdd6fe09b7a
astadipati <astadipati@otlook.com>
2021-07-28 04:28:17 +0700

    asynchandler DRY

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -2,6 +2,9 @@
 // const ErrorResponse = require('../helper/errorResponse');
 // kita load models
 const ErrorResponse = require('../helper/errorResponse');
+// kita terapkan DRY Dont Repeat Yourself untuk error handling
+// middleware ini akan menggantikan try catch berulang
+const asyncHandler = require('../middleware/async');
 const Bootcamp = require('../models/Bootcamp');
 
 // dalam controller = method
@@ -9,25 +12,20 @@ const Bootcamp = require('../models/Bootcamp');
 // @route   GET /api/v1/bootcamp
 // @access  Public
 
-exports.getBootcamps = async (req, res, next) =>{ //midleware function
-    try {
+exports.getBootcamps = asyncHandler(async (req, res, next) =>{ //midleware function
+    
         const bootcamps = await Bootcamp.find();
         res
             .status(200)
             .json({success: true, jml: bootcamps.length, data: bootcamps}); //sekaligus akses middleware logger
-    } catch (err) {
-        next(err);
-        // res
-        //     .status(400)
-        //     .json({success:false});
-    }
-}; 
+    
+}); 
 // @desc    Get single bootcamps
 // @route   GET /api/v1/bootcamp/:id
 // @access  Public
 
-exports.getBootcamp = async (req, res, next) =>{ //midleware function
-    try {
+exports.getBootcamp = asyncHandler(async (req, res, next) =>{ //midleware function
+    // try {
         const bootcamp = await Bootcamp.findById(req.params.id);
 
         // jika sesuai format tapi tidak ada data
@@ -42,44 +40,44 @@ exports.getBootcamp = async (req, res, next) =>{ //midleware function
         res
             .status(200)
             .json({success: true, data: bootcamp});
-    } catch (err) {
-        next(err);
+    // } catch (err) {
+        // next(err);
         // res
         //     .status(400)
         //     .json({success:false});
         // next(new ErrorResponse(`Bootcamp nor found with id of ${req.params.id}`, 404));
-    }
+    // }
     // res.status(200).json({success: true, msg: `get single bootcamps ${req.params.id}`});
-};  
+});  
 
 // @desc    Create a new bootcamp
 // @route   POST /api/v1/bootcamp
 // @access  Private after login
 
-exports.createBootcamp = async (req, res, next) =>{ //midleware function
-    try {
+exports.createBootcamp = asyncHandler(async (req, res, next) =>{ //midleware function
+    // try {
         const bootcamp = await Bootcamp.create(req.body);
         res.status(201).json({
             success: true,
             data: bootcamp
         });
         
-    } catch (err) {
-        next(err);
+    // } catch (err) {
+    //     next(err);
         // res
         // .status(400)
         // .json({success:false, msg:`Cek duplikat`});
-    }
+    // }
     // console.log(req.body);
     // res.status(200).json({success: true, msg: 'Create new bootcamps'});
-};  
+});  
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamp:id
 // @access  Private
 
-exports.updateBootcamp = async (req, res, next) =>{ //midleware function
-    try {
+exports.updateBootcamp = asyncHandler(async (req, res, next) =>{ //midleware function
+    // try {
         const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body,{
             new: true,
             runValidators: true
@@ -92,21 +90,20 @@ exports.updateBootcamp = async (req, res, next) =>{ //midleware function
                 new ErrorResponse(`ID tidak ditemukan ${req.params.id}`, 404)
             )
         }
-    } catch (error) {
-        next(err);
+    // } catch (error) {
+    //     next(err);
         // res
         // .status(400)
         // .json({success:false, msg:`Gagal Melakukan Update ${req.params.id}`});
-    }
-};
+    // }
+});
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamp/:id
 // @access  private
 
-exports.deleteBootcamp =async (req, res, next) =>{ //midleware function
-    try {
+exports.deleteBootcamp =asyncHandler(async (req, res, next) =>{ //midleware function
+    // try {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
-
         res.status(200).json({success: true, msg: `Delete bootcamps ${req.params.id}`});
         if (!bootcamp) {
             return next(
@@ -115,10 +112,10 @@ exports.deleteBootcamp =async (req, res, next) =>{ //midleware function
             )
             // return res.status(400).json({success: false, msg:`Gagal Hapus Data`});
         }
-    } catch (error) { 
-        next(err);
+    // } catch (error) { 
+    //     next(err);
         // res
         // .status(400)
         // .json({success:false, msg:`Gagal Menghapus Data ${req.params.id}`});
-    }
-};  
+    // }
+});  
