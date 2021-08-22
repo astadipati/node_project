e628bcbf6fcb5cb00af274856b36b0be961e398d
astadipati <astadipati@otlook.com>
2021-08-22 19:56:16 +0700

    Import Model Courses

--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ const dotenv = require ('dotenv');
 dotenv.config ({path: './config/config.env'});
 
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 // konek db
 mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
@@ -17,11 +18,13 @@ mongoose.connect(process.env.MONGO_URI,{
 
 // read json files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8'));
 
 // import into db
 const importData = async() =>{
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
         console.log('Data imported...'.green.inverse);
         process.exit();
     } catch (err) {
@@ -33,6 +36,7 @@ const importData = async() =>{
 const deleteData = async() =>{
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
     } catch (err) {
