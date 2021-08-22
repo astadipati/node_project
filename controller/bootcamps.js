9e1d6e27b9ec9638f2c54be102b0fb97e2673532
astadipati <astadipati@otlook.com>
2021-08-22 07:53:54 +0700

    Pagination

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -21,7 +21,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) =>{ //midleware funct
     const reqQuery = {...req.query};
 
     // Fields to exclude
-    const removeFields = ['select','sort'];
+    const removeFields = ['select', 'sort', 'page', 'limit'];
 
     // Loop over removeFields and delete them from reqQuery
     removeFields.forEach(param => delete reqQuery[param]);
@@ -49,15 +49,46 @@ exports.getBootcamps = asyncHandler(async (req, res, next) =>{ //midleware funct
         const sortBy = req.query.sort.split(',').join(' ');
         query = query.sort(sortBy);
     }else{
+        // default sorting jika tidak berdasar apapun
         query = query.sort('-createdAt');
     }
 
-    // const bootcamps = await Bootcamp.find(); berubah menunggu query
+    // pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 2;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Bootcamp.countDocuments();
+  
+    query = query.skip(startIndex).limit(limit);
+    
+    // const resultQuery = 
+
+    // const bootcamps = await Bootcamp.find(); 
     // executing query
     const bootcamps = await query;
-        res
-            .status(200)
-            .json({success: true, jml: bootcamps.length, data: bootcamps}); //sekaligus akses middleware logger
+
+    // pagination result
+    const pagination = {};
+
+    if (endIndex < total) {
+        pagination.next = {
+        page: page + 1,
+        limit
+        };
+    }
+
+    if (startIndex > 0) {
+        pagination.prev = {
+        page: page - 1,
+        limit
+        };
+    }
+    
+
+    res
+        .status(200)
+        .json({success: true, jml: bootcamps.length, pagination, data: bootcamps}); //sekaligus akses middleware logger
     
 }); 
 // @desc    Get single bootcamps
